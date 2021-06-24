1139afd7da874f10d1bb256efbb42cd14259fb03
jonnycca <jonnyccalves@gmail.com>
2021-06-24 13:47:05 -0300

    estructure initial

---
+++ src/main/java/br/com/cleanproject/CleanprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cleanproject/CleanprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleanproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
